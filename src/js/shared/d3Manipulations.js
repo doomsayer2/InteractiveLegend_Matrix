f03d1b58e07ce6ad083fca5d75ecd5b50dbbeac0
Florian <fgrassinger@gmx.at>
2019-10-26 14:05:24 +0200

    Add hint 5 to the viz. (refs #1)

--- src/js/shared/d3Manipulations.js
+++ src/js/shared/d3Manipulations.js
@@ -145,6 +145,52 @@ const createHintFour = () => {
 };
 
 const createHintFive = () => {
+  const tallinn = d3
+    .select('.vegaViz1 > svg')
+    .selectAll('.role-axis-label')
+    .nodes()[1];
+  const tallinnText = d3.select(tallinn).select('text:nth-child(3)');
+  const xAxisLabel = d3
+  .select('.vegaViz1 > svg')
+  .selectAll('.role-axis-label')
+  .nodes()[0];
+  const sept = d3.select(xAxisLabel).select('text:nth-child(9)');
+  const oct = d3.select(xAxisLabel).select('text:nth-child(10)');
+  const nov = d3.select(xAxisLabel).select('text:nth-child(11)');
+  const dez = d3.select(xAxisLabel).select('text:nth-child(12)');
+  const dezTransform = dez.attr('transform');
+  let dezBox = dezTransform.substring(dezTransform.indexOf('(') + 1, dezTransform.indexOf(')'))
+  .split(',');
+  dezBox = dezBox.map(e => parseInt(e, 0));
+
+// febBox = febBox.map(e => parseInt(e, 0));
+  tallinnText.style('fill', '#C51B7D').attr('font-weight', 'bold');
+  sept.style('fill', '#C51B7D').attr('font-weight', 'bold');
+  oct.style('fill', '#C51B7D').attr('font-weight', 'bold');
+  nov.style('fill', '#C51B7D').attr('font-weight', 'bold');
+  dez.style('fill', '#C51B7D').attr('font-weight', 'bold');
+
+  const hint5Group = d3
+    .select('.vegaViz1 > svg')
+    .select('.role-axis-label')
+    .append('g')
+    .classed('customD3Hints', true);
+
+  hint5Group
+    .append('circle')
+    .attr('r', 10)
+    .attr('cx', dezBox[0] - 18)
+    .attr('cy', dezBox[1] - 30)
+    .style('stroke', '#C51B7D')
+    .style('fill', '#C51B7D');
+  hint5Group
+    .append('text')
+    .attr('x', dezBox[0] - 18)
+    .attr('y', dezBox[1] - 25)
+    .attr('text-anchor', 'middle')
+    .attr('fill', 'white')
+    .text('5');
+
   return 5;
 };
 
@@ -158,12 +204,10 @@ const removeAllHints = () => {
   // Custom Changes to the elements itself and not additions
   const xAxisText = d3.select(d3.selectAll('.role-axis-title').nodes()[0]);
   const yAxisText = d3.select(d3.selectAll('.role-axis-title').nodes()[1]);
-
   xAxisText.select('text').style('fill', 'rgb(0,0,0)');
   yAxisText.select('text').style('fill', 'rgb(0,0,0)');
 
   const legend = d3.select('.vegaViz1 > svg').select('.role-legend-title');
-
   legend
     .select('text')
     .style('fill', 'rgb(0,0,0)')
@@ -184,9 +228,28 @@ const removeAllHints = () => {
       .selectAll('text')
       .nodes()[1]
   );
-
   munichText.style('fill', 'rgb(0,0,0)').attr('font-weight', 'normal');
   febText.style('fill', 'rgb(0,0,0)').attr('font-weight', 'normal');
+
+  const tallinn = d3
+    .select('.vegaViz1 > svg')
+    .selectAll('.role-axis-label')
+    .nodes()[1];
+  const tallinnText = d3.select(tallinn).select('text:nth-child(3)');
+  tallinnText.style('fill', 'rgb(0,0,0)').attr('font-weight', 'normal');
+
+  const xAxisLabel = d3
+  .select('.vegaViz1 > svg')
+  .selectAll('.role-axis-label')
+  .nodes()[0];
+  const sept = d3.select(xAxisLabel).select('text:nth-child(9)');
+  const oct = d3.select(xAxisLabel).select('text:nth-child(10)');
+  const nov = d3.select(xAxisLabel).select('text:nth-child(11)');
+  const dez = d3.select(xAxisLabel).select('text:nth-child(12)');
+  sept.style('fill', 'rgb(0,0,0)').attr('font-weight', 'normal');
+  oct.style('fill', 'rgb(0,0,0)').attr('font-weight', 'normal');
+  nov.style('fill', 'rgb(0,0,0)').attr('font-weight', 'normal');
+  dez.style('fill', 'rgb(0,0,0)').attr('font-weight', 'normal');
 };
 
 export {
