324036aa03a89a5dce5cd3ca9f7d18f73f05f463
NChristina <christina.stoiber@fhstp.ac.at>
2019-10-30 14:52:57 +0100

    adapt highlighting intro sentence (ref #1)

--- src/js/shared/constants.js
+++ src/js/shared/constants.js
@@ -5,4 +5,4 @@ export const PAGE_SUBTITLE =
   'St. Pölten University of Applied Sciences, Austria';
 export const VIZ_TITLE = 'For the Title';
 export const VIZ_DESC =
-  'The change matrix shows the absolute difference of <span class="hT">average temperatures</span> in <span class="hT">Munich</span>, <span class="hT">Oslo</span>, and <span class="hT">Tallinn between 1990</span> and <span class="hT">1991</span>. The absolute differences between the two years and the average temperature are indicated by a <span class="hT">color gradient</span>.';
+  'The change matrix shows the absolute difference of <span class="hT">average temperatures</span> in <span class="hT">Munich</span>, <span class="hT">Oslo</span>, and <span class="hT">Tallinn</span> <span class="hT">between 1990 and 1991</span>. The absolute differences between the two <span class="hT">years</span> and the <span class="hT">average temperature</span> are indicated by a color gradient.';
